db228570c539dd87cd148077147a025a41ca58ba
PonyMyy <149934422+PonyMyy@users.noreply.github.com>
2024-01-13 18:42:04 +0800

    Delete 大一上C语言 directory

--- 大一上C语言/C语言实验2/实验2 验证性例2.17.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	char a[10];
-	scanf("%5s",a);
-	printf("%s",a);
-	return 0;
- } 
--- 大一上C语言/C语言实验3/实验3 验证性例4.9.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-	int a,b,max;
-	printf("请输入两个整数：\n");
-	scanf("%d,%d",&a,&b);
-	printf("二者中较大的整数是：\n%d\n",(max=(a>b)?a:b));
-	return 0;
-}
